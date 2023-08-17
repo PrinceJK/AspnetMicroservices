c8cb021960a6c8a4ae4fa3a533831cbdfdf7de92
John Kehinde <kehindejohn018@gmail.com>
2023-08-17 15:11:07 +0100

    Added enties for the basket api

---
+++ Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; }
+        public List<ShoppingCartItem> Items { get; set; }
+
+        public ShoppingCart()
+        {
+            Items = new List<ShoppingCartItem>();
+        }
+
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+            Items = new List<ShoppingCartItem>();
+        }
+
+        public decimal TotalPrice
+        {
+            get { return Items.Sum(item => item.Price * item.Quality); }
+        }
+    }
+}
